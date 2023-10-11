ae4b9ff4a12dcb8fe795329745e6c43e2f73de07
Quan Zheng <q.zheng-7@student.tudelft.nl>
2023-10-11 22:43:59 +0300

    Remove console log from PostBody.jsx

--- components/posts/PostBody.jsx
+++ components/posts/PostBody.jsx
@@ -3,7 +3,6 @@ import RichText from '../RichText'
 const PostBody = ({ post }) => {
   const { content } = post.fields
 
-  console.log(content)
   return (
     <div className='mx-auto prose'>
       <RichText content={content} />
