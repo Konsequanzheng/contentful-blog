ca5e941ee736714759d5b4ff73da5dceb17d0fb8
Quan Zheng <q.zheng-7@student.tudelft.nl>
2023-10-11 20:47:43 +0300

    Add styling and rendering of posts page

--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { client } from '@/lib/contentful/client'
+import PostCard from '@/components/posts/PostCard'
 
 const Posts = () => {
-  return <div>Posts</div>
+  return (
+    <section className='section'>
+      <div className='container'>
+        <ul className='grid grid-cols-1 sm:grid-cols-2 lg:gridcols-3 sm:gap-10'>
+          {Posts.map((post, i) => (
+            <PostCard key={post.fields.slug} post={post} />
+          ))}
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export const getStaticProps = async () => {
+  const response = await client.getEntries({ content_type: 'post' })
+
+  return {
+    props: {
+      posts: response.items,
+      revalidate: 60
+    }
+  }
 }
 
 export default Posts
